e9fb20b315d2f2af7c9a3a204c7bec6e42fe26fa
Syah Riza <rezaqt@gmail.com>
2022-10-11 01:58:41 +0700

    feat: add footer

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,7 +1,24 @@
 import React from "react";
-
+import { FaInstagram, FaLinkedin, FaFacebookSquare } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
 const Footer = () => {
-  return <div className="bg-white-300 pt-44 pb-24"></div>;
+  return (
+    <>
+      <div className="bg-transparent flex flex-col justify-center py-4 h-32 items-center text-green-600">
+        <p className="text-black">Contact Us</p>
+        <div className="flex space-x-4 pb-8 pt-6">
+          <FaInstagram size={30} /> <FaLinkedin size={30} />
+          <FaFacebookSquare size={30} />
+          <HiOutlineMail size={30} />
+        </div>
+      </div>
+      <div className="bg-green-500 flex justify-center py-4 h-16 items-center">
+        <h2 className="text-semibold text-md text-white">
+          &copy;ECO AERATOR 2022
+        </h2>
+      </div>
+    </>
+  );
 };
 
 export default Footer;
