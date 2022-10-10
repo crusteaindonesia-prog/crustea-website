7dcb714a57af6cca5503bb3b1410b902d27f8c5c
Syah Riza <rezaqt@gmail.com>
2022-10-10 08:42:18 +0700

    feature initial

---
+++ src/app/app.tsx
@@ -0,0 +1,23 @@
+
+import styles from "./app.module.css";
+
+
+const App = (): JSX.Element => {
+  return (
+    <main className={styles.main}>
+      <header className={styles.header}>
+        <h3 className={styles.headerTopTitle}>
+          
+          Crustea.id
+        </h3>
+        
+      </header>
+      
+      <footer className={styles.footer}>
+        
+      </footer>
+    </main>
+  );
+};
+
+export default App;
