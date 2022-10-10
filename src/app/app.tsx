21aac13bad953a8c6f68074e7f4c5cf9ef1b4cf9
Syah Riza <rezaqt@gmail.com>
2022-10-11 01:45:19 +0700

    feat: add testimonial

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,10 +1,12 @@
 import Hero from "components/Hero";
+import Testimoni from "components/Testimoni";
 import Layout from "../components/Layout/Layout";
 
 const App = (): JSX.Element => {
   return (
     <Layout>
       <Hero />
+      <Testimoni />
     </Layout>
   );
 };
