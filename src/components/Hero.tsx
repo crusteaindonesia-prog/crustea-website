7e525328311da48e57059bf5cd146f70f5098d2d
laodeshaldanfalih <77shaldan77@gmail.com>
2023-07-31 21:49:08 +0700

    feat: add company purpose

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -59,6 +59,33 @@ const Hero = ({
           </div>
         </motion.div>
       </ScrollAnimationWrapper>
+      <div
+        className="flex flex-col items-center main-h-screen mt-24 mb-24"
+        id="company-purpose"
+      >
+        <ScrollAnimationWrapper className={undefined}>
+          <motion.div variants={scrollAnimation}>
+            <h3 className="text-green-600 pt-10 font-sans font-bold text-2xl md:text-4xl py-9 text-center">
+              Tujuan utama kami membantu petani tambak Indonesia
+            </h3>
+            <p className="txt-base text-center">
+              Sektor akuakultur Indonesia merupakan salah satu sektor yang
+              paling berpotensi untuk menjadi penopang ekonomi nasional saat ini
+              mengingat melimpahnya sumber daya alam yang mampu menunjang siklus
+              kehidupan biota akuakultur di dalamnya. Namun, masih banyak
+              kendala yang dihadapi dalam sektor akuakultur yang dapat
+              menghambat proses budidaya. <br /> <br />
+              Berawal dari suara keresahan para petani tambak Indonesia yang
+              mengeluhkan tingginya biaya operasional dalam menjalankan budidaya
+              akuakultur yang berdampak pada kurangnya produktivitas petambak
+              Indonesia, kami berkomitmen untuk menciptakan inovasi teknologi
+              yang mampu membantu para petani mendapatkan hasil yang maksimal,
+              sekaligus berkontribusi langsung untuk melindungi bumi dengan
+              inovasi kami yang ramah lingkungan.
+            </p>
+          </motion.div>
+        </ScrollAnimationWrapper>
+      </div>
       <div className="relative w-full flex">
         <ScrollAnimationWrapper className="rounded-lg w-full grid grid-flow-row sm:grid-flow-row grid-cols-1 sm:grid-cols-3 py-9 divide-y-2 sm:divide-y-0 sm:divide-x-2 divide-gray-100 bg-white-500 z-10">
           {listUser.map((listUsers, index) => (
