866af3f1e995e8f3ea2ad1a6c6dd4092e0543524
IAmNewbe <ahlulhikam2222@gmail.com>
2023-08-17 05:26:28 +0700

    fix: wont load in sm device

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -48,47 +48,51 @@ const Hero = () => {
       >
         <ScrollAnimationWrapper className={undefined}>
           <motion.div variants={scrollAnimation}>
-            <h3 className="text-green-600 pt-5 md:pt-10 font-sans font-bold text-3xl md:text-5xl py-9 text-center">
+            <h3 className="opacity-100 text-green-600 pt-5 md:pt-10 font-sans font-bold text-3xl md:text-5xl py-9 text-center">
               Uncover <span style={{ color: "#ACCB37" }}>Our Story</span>
             </h3>
-            <div className="flex flex-wrap">
-              <div className="w-full lg:w-1/2 mx-5 md:mx-auto">
-                <img
-                  className="rounded-2xl mx-auto md:rounded-3xl w-full md:max-w-[568px] lg:mr-14 justify-start lg:max-h-[550px] object-cover"
-                  alt=""
-                  src={aboutImg}
-                ></img>
-              </div>
-              <div className="w-full lg:w-1/2">
-                <div className="mx-5 lg:ml-14 lg:max-w-[510px]">
-                  <h3 className="pt-6 lg:pt-0 font-semibold text-2xl md:text-4xl font-sans ">
-                    We're Dedicated to{" "}
-                    <span style={{ color: "#ACCB37" }}>
-                      Enhancing the Productivity
-                    </span>{" "}
-                    of Indonesian Pond Farmers.
-                  </h3>
-                  <p className="font-sans text-[17px] pt-6 text-justify lg:text-left lg:pt-14 text-black-600">
-                    Driven by the concerns of Indonesian pond farmers who
-                    struggle with high operational costs impacting productivity,
-                    we have taken a pledge to innovate technology solutions.
-                  </p>
-                  <p className="font-sans text-[17px] pt-3 text-justify lg:text-left lg:pt-10 text-black-600">
-                    Driven by the concerns of Indonesian pond farmers who
-                    struggle with high operational Our goal is to empower
-                    farmers to achieve the best possible outcomes in terms of
-                    quantity and quality in aquaculture production while making
-                    a direct contribution to environmental preservation through
-                    eco-friendly innovations.
-                  </p>
-                  <button className="bg-[#ACCB37] rounded-3xl text-white-300 lg:mt-14 py-3 px-6 shadow-xl font-semibold text-xl hover:bg-white-300 hover:text-[#ACCB37] ring-[#ACCB37] ring-2 mt-6">
-                    Learn More
-                  </button>
-                </div>
-              </div>
-            </div>
           </motion.div>
         </ScrollAnimationWrapper>
+        <div className="flex flex-wrap">
+          <ScrollAnimationWrapper className="w-full lg:w-1/2 mx-5 md:mx-auto">
+            <motion.div variants={scrollAnimation}>
+              <img
+                className="rounded-2xl mx-auto md:rounded-3xl w-full md:max-w-[568px] lg:mr-14 justify-start lg:max-h-[550px] object-cover"
+                alt=""
+                src={aboutImg}
+              ></img>
+            </motion.div>
+          </ScrollAnimationWrapper>
+          <ScrollAnimationWrapper className="w-full lg:w-1/2">
+            <motion.div
+              variants={scrollAnimation}
+              className="mx-5 lg:ml-14 lg:max-w-[510px]"
+            >
+              <h3 className="pt-6 lg:pt-0 font-semibold text-2xl md:text-4xl font-sans ">
+                We're Dedicated to{" "}
+                <span style={{ color: "#ACCB37" }}>
+                  Enhancing the Productivity
+                </span>{" "}
+                of Indonesian Pond Farmers.
+              </h3>
+              <p className="font-sans text-[17px] pt-6 text-justify lg:text-left lg:pt-14 text-black-600">
+                Driven by the concerns of Indonesian pond farmers who struggle
+                with high operational costs impacting productivity, we have
+                taken a pledge to innovate technology solutions.
+              </p>
+              <p className="font-sans text-[17px] pt-3 text-justify lg:text-left lg:pt-10 text-black-600">
+                Driven by the concerns of Indonesian pond farmers who struggle
+                with high operational Our goal is to empower farmers to achieve
+                the best possible outcomes in terms of quantity and quality in
+                aquaculture production while making a direct contribution to
+                environmental preservation through eco-friendly innovations.
+              </p>
+              <button className="bg-[#ACCB37] rounded-3xl text-white-300 lg:mt-14 py-3 px-6 shadow-xl font-semibold text-xl hover:bg-white-300 hover:text-[#ACCB37] ring-[#ACCB37] ring-2 mt-6">
+                Learn More
+              </button>
+            </motion.div>
+          </ScrollAnimationWrapper>
+        </div>
       </div>
       <div className="main-h-screen mt-24 mb-24" id="company-purpose"></div>
     </div>
