21474a0f9de5ce7b3331c5dc7ef5f6b47cb25014
laodeshaldanfalih <77shaldan77@gmail.com>
2023-08-21 09:43:52 +0700

    fix: vision font family and title padding

--- src/components/Vision.tsx
+++ src/components/Vision.tsx
@@ -19,7 +19,7 @@ const Vision = () => {
               style={{ backgroundImage: `url(${imgBG})` }}
             >
               <div className="bg-[#ACCB37] h-full lg:h-full w-full mix-blend-multiply">
-                <h2 className="text-white-300 h-full lg:h-full font-bold mx-auto mix-blend-normal flex items-center justify-center text-3xl lg:text-6xl">
+                <h2 className="text-white-300 h-full lg:h-full font-Cabin font-bold mx-auto mix-blend-normal flex items-center justify-center text-3xl lg:text-6xl">
                   OUR VISION
                 </h2>
               </div>
@@ -33,7 +33,7 @@ const Vision = () => {
             className="w-full lg:w-[3/5] md:h-72 md:basis-3/5"
           >
             <div className="bg-[#DEE8DB] lg:py-16 h-32 w-full lg:h-72">
-              <p className="text-xl text-[#000000] h-full lg:text-3xl max-w-xl mx-5 sm:mx-10 md:mx-auto text-center font-medium flex items-center lg:leading-[48px] md:justify-center italic lg:text-right lg:mr-16">
+              <p className="text-xl text-[#000000] h-full lg:text-3xl max-w-xl mx-5 sm:mx-10 md:mx-auto font-DMSans text-center font-medium flex items-center lg:leading-[48px] md:justify-center italic lg:text-right lg:mr-16">
                 Increasing the Pond Farmer's Productivity & Food Security by
                 Implementing Eco-Aerator Technology
               </p>
