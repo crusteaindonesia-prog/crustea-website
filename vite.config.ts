32a0785afa40f27b080e5811fe165006c519c4e9
crusteaindonesia-prog <crusteaindonesia@gmail.com>
2026-02-25 13:12:29 +0700

    Change base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig((configEnv) => {
 
   return {
     plugins: [react()],
-    base: "/crustea-website/", // Set the base path for the project on GitHub Pages
+    base: "/", // Set the base path for the project on GitHub Pages
     resolve: {
       alias: {
         app: resolve(__dirname, "src", "app"),
